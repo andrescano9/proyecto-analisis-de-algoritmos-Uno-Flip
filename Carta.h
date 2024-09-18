279db8bfcfcdfacec77e9d5fde8d0f8f9f82028c
andrescano9 <110790627+andrescano9@users.noreply.github.com>
2024-09-18 17:36:16 -0500

    Create Carta.h

---
+++ Carta.h
@@ -0,0 +1,15 @@
+#ifndef CARTA_H
+#define CARTA_H
+
+#include <string>
+using namespace std;
+
+class Carta {
+public:
+    string color;
+    string valor;
+
+    Carta(string c, string v);
+};
+
+#endif
