35fb3587b2e94f2903b3046770e1e339e7185ee1
andrescano9 <110790627+andrescano9@users.noreply.github.com>
2024-09-24 16:56:40 -0500

    Update Mazo.h

--- Mazo.h
+++ Mazo.h
@@ -5,35 +5,21 @@
 #include <algorithm> // para barajar
 #include "Carta.h"
 
-// Mazo.h
-
+// Clase Mazo
 class Mazo {
 private:
-    vector<Carta> cartas; // Cartas del mazo
-    Carta cartaActiva;    // Carta activa en la mesa
+    vector<Carta> cartas;        // Cartas del mazo
+    vector<Carta> mazoDescarte;  // Cartas descartadas
+    Carta cartaActiva;           // Carta activa en la mesa
 
 public:
     Mazo(); // Constructor
     void barajar();
     Carta sacarCarta();
+    void devolverCartaDescarte(const Carta& carta); // Devolver carta al mazo de descarte
     void setCartaActiva(const Carta& carta); // Establece la carta activa
     Carta getCartaActiva() const;           // Devuelve la carta activa
     int getNumCartas() const;                // Devuelve el número de cartas en el mazo
 };
 
-// Mazo.cpp
-
-void Mazo::setCartaActiva(const Carta& carta) {
-    cartaActiva = carta;
-}
-
-Carta Mazo::getCartaActiva() const {
-    return cartaActiva;
-}
-
-int Mazo::getNumCartas() const {
-    return cartas.size();
-}
-
-
-#endif
+#endif 
