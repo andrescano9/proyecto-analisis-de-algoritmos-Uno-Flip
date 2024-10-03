75ee9ef88a39e96ac531389118b9b8eaf694d578
andrescano9 <110790627+andrescano9@users.noreply.github.com>
2024-10-02 19:15:42 -0500

    Update Jugador.h

--- Jugador.h
+++ Jugador.h
@@ -1,16 +1,27 @@
+// Jugador.h
+
 #ifndef JUGADOR_H
 #define JUGADOR_H
 
 #include <vector>
+#include <string>
 #include "Carta.h"
 
 class Jugador {
 public:
-    string nombre;
-    vector<Carta> mano;
+    std::string nombre;
+    std::vector<Carta> mano;
 
-    Jugador(string n);
-    void agregarCarta(Carta carta);
+    // Constructor que inicializa el nombre del jugador
+    Jugador(std::string nombreJugador) : nombre(nombreJugador) {}
+
+    // Método que agrega una carta a la mano del jugador
+    void agregarCarta(const Carta& carta) {
+        mano.push_back(carta);
+    }
+
+    // Método que muestra las cartas en la mano del jugador con un parámetro
+    void mostrarMano(bool isLight) const; // Modificar aquí para aceptar un argumento booleano
 };
 
-#endif
+#endif // JUGADOR_H
